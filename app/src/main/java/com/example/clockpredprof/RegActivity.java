ce5dd44a0a6771323c2c54b5e28c851fe10d6c86
DanilPustovalov <Kir_Fall@mail.ru>
2022-02-20 20:02:21 +0300

    Добавление активности с регистрацией, данные из которой уходят в базу данных

---
+++ app/src/main/java/com/example/clockpredprof/RegActivity.java
@@ -0,0 +1,40 @@
+package com.example.clockpredprof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+/**
+ * Класс активности для регистрации пользователя.
+ * @autor Пустовалов Данил
+ */
+public class RegActivity extends AppCompatActivity {
+    /** Поле Менеджера базы данных */
+    static DBManager dbManager;
+    /** Поля дизайна */
+    EditText mail;
+    EditText name1;
+    Button con;
+    /** При создании приложения */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+        con=findViewById(R.id.Continue);
+        mail=findViewById(R.id.Mail);
+        name1=findViewById(R.id.NameForReg);
+        con.setOnClickListener(new View.OnClickListener() {
+            /** При нажатии на кнопку */
+            @Override
+            public void onClick(View v) {
+                dbManager.addResult(RegActivity.this.name1.getText().toString(),
+                        RegActivity.this.mail.getText().toString());
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
